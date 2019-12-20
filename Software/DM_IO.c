e3ae1262e0ddeedcebae95d6519c7571b9d15ba2
Finn <fcyriax@web.de>
2019-12-20 11:11:45 +0100

    Basic Code for LEDs

--- Software/DM_IO.c
+++ Software/DM_IO.c
@@ -1,13 +1,28 @@
 /*
  * DM_IO.c
  *
+ *  This is the Main IO Functions of the Drum Maschine
+ * 
+ *  
+ * 
  *  Created on: 14.12.2019
  *      Author: Finn Cyriax
  */
 
-#include <alt_types.h>
-#include <altera_up_avalon_parallel_port.h>
-
 #include "DM_IO.h"
 
 
+void init() {
+
+
+}
+
+
+int main(void) {
+    alt_up_parallel_port_dev *greenLEDs, *redLEDs;
+
+    greenLEDs = alt_up_parallel_port_open_dev("/dev/green_leds");
+    redLEDs = alt_up_parallel_port_open_dev("/dev/red_leds");
+
+
+}
--- Software/DM_IO.h
+++ Software/DM_IO.h
@@ -1,6 +1,9 @@
 /*
  * DM_IO.h
  *
+ *  This is the Header File for the Main IO of the Drum Maschine.
+ *  It contains all includes and defines.
+ *  
  *  Created on: 14.12.2019
  *      Author: Finn Cyriax
  */
@@ -8,7 +11,14 @@
 #ifndef DM_IO_H_
 #define DM_IO_H_
 
+// Includes
+
 #include <system.h>
-#include <altera_avalon_parallel_port.h>
+#include <alt_types.h>
+#include <altera_up_avalon_parallel_port.h>
+
+// Defines
+
+
 
 #endif /* DM_IO_H_ */
